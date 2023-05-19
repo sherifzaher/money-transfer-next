6f65f73d275fb7a73ee38ee4589dc768bcbd0f01
sherifzaher <sherifashrafmedia@gmail.com>
2023-05-19 20:16:03 +0300

    Initial Commit

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import {Html, Head, NextScript, Main} from "next/document";
+
+export default function Document(){
+  return(
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
